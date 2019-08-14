8c2d85b042d9899f77c37ad8c4e024dc744f0fa5
冯鑫龙 <18862005869@163.com>
2019-08-14 14:37:30 +0800

    删除无用信息

--- myPython/myPython/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def hello(request):
-    content = {}
-    content['hello'] = "hello world!"
-    return render(request, 'hello.html', content)
-    # return HttpResponse("hello World ! ")
